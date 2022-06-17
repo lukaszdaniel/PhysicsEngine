9e8b173103d5f7b8984eb1ca5b3bd335cc70ac82
Lukasz Daniel <lukasz.daniel@gmail.com>
2022-06-17 22:11:19 +0200

    State class now has private section.

--- include/engine/render/viewport_handler.hpp
+++ include/engine/render/viewport_handler.hpp
@@ -28,6 +28,57 @@ public:
             m_mouse_world_position = m_offset + (pos - m_center) / m_zoom;
         }
 
+        void addOffset(sf::Vector2f v)
+        {
+            m_offset += v / m_zoom;
+        }
+
+        void zoom(float f)
+        {
+            m_zoom *= f;
+        }
+
+        const sf::Transform &transform() const
+        {
+            return m_transform;
+        }
+
+        bool clicking() const
+        {
+            return m_clicking;
+        }
+
+        void setClicking(bool on)
+        {
+            m_clicking = on;
+        }
+
+        sf::Vector2f mouseWorldPosition() const
+        {
+            return m_mouse_world_position;
+        }
+
+        sf::Vector2f center() const
+        {
+            return m_center;
+        }
+
+        sf::Vector2f mousePosition() const
+        {
+            return m_mouse_position;
+        }
+
+        void setMousePosition(sf::Vector2f new_position)
+        {
+            m_mouse_position = new_position;
+        }
+
+        void setOffset(sf::Vector2f val)
+        {
+            m_offset = val;
+        }
+
+    private:
         sf::Vector2f m_center;
         sf::Vector2f m_offset;
         float m_zoom;
@@ -45,13 +96,13 @@ public:
 
     void addOffset(sf::Vector2f v)
     {
-        m_state.m_offset += v / m_state.m_zoom;
+        m_state.addOffset(v);
         m_state.updateState();
     }
 
     void zoom(float f)
     {
-        m_state.m_zoom *= f;
+        m_state.zoom(f);
         m_state.updateState();
     }
 
@@ -67,55 +118,55 @@ public:
 
     void reset()
     {
-        m_state.m_zoom = 1.0f;
-        setFocus(m_state.m_center);
+        m_state.zoom(1);
+        setFocus(m_state.center());
     }
 
     const sf::Transform &getTransform() const
     {
-        return m_state.m_transform;
+        return m_state.transform();
     }
 
     void click(sf::Vector2f relative_click_position)
     {
-        m_state.m_mouse_position = relative_click_position;
-        m_state.m_clicking = true;
+        m_state.setMousePosition(relative_click_position);
+        m_state.setClicking(true);
     }
 
     void unclick()
     {
-        m_state.m_clicking = false;
+        m_state.setClicking(false);
     }
 
     void setMousePosition(sf::Vector2f new_mouse_position)
     {
-        if (m_state.m_clicking)
+        if (m_state.clicking())
         {
-            addOffset(m_state.m_mouse_position - new_mouse_position);
+            addOffset(m_state.mousePosition() - new_mouse_position);
         }
         m_state.updateMousePosition(new_mouse_position);
     }
 
     void setFocus(sf::Vector2f focus_position)
     {
-        m_state.m_offset = focus_position;
+        m_state.setOffset(focus_position);
         m_state.updateState();
     }
 
     void setZoom(float new_zoom)
     {
-        m_state.m_zoom = new_zoom;
+        m_state.zoom(new_zoom);
         m_state.updateState();
     }
 
     sf::Vector2f getMouseWorldPosition() const
     {
-        return m_state.m_mouse_world_position;
+        return m_state.mouseWorldPosition();
     }
 
     sf::Vector2f getScreenCoords(sf::Vector2f world_pos) const
     {
-        return m_state.m_transform.transformPoint(world_pos);
+        return m_state.transform().transformPoint(world_pos);
     }
 
 private:
