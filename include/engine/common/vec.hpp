7f56612c683cbda3743493c7a1eeb328dc7afd8c
Lukasz Daniel <lukasz.daniel@gmail.com>
2022-06-17 22:01:56 +0200

    Class Vec2 is unused.

--- include/engine/common/vec.hpp
+++ include/engine/common/vec.hpp
@@ -1,21 +1,22 @@
 #pragma once
 #include <cmath>
-
-struct Vec2
+#if 0 // UNUSED
+class Vec2
 {
+public:
 	Vec2()
-		: x(0.0f), y(0.0f)
+		: m_x(0.0f), m_y(0.0f)
 	{
 	}
 
 	Vec2(float x_, float y_)
-		: x(x_), y(y_)
+		: m_x(x_), m_y(y_)
 	{
 	}
 
 	float getLength2() const
 	{
-		return x * x + y * y;
+		return m_x * m_x + m_y * m_y;
 	}
 
 	float getLength() const
@@ -26,56 +27,56 @@ struct Vec2
 	Vec2 operator/(float f) const
 	{
 		const float inv = 1.0f / f;
-		return Vec2(x * inv, y * inv);
+		return Vec2(m_x * inv, m_y * inv);
 	}
 
 	Vec2 operator*(float f) const
 	{
-		return Vec2(x * f, y * f);
+		return Vec2(m_x * f, m_y * f);
 	}
 
 	Vec2 operator-(const Vec2 &other) const
 	{
-		return Vec2(x - other.x, y - other.y);
+		return Vec2(m_x - other.m_x, m_y - other.m_y);
 	}
 
 	Vec2 operator-() const
 	{
-		return Vec2(-x, -y);
+		return Vec2(-m_x, -m_y);
 	}
 
 	void operator+=(const Vec2 &other)
 	{
-		x += other.x;
-		y += other.y;
+		m_x += other.m_x;
+		m_y += other.m_y;
 	}
 
 	void operator+=(float f)
 	{
-		x += f;
-		y += f;
+		m_x += f;
+		m_y += f;
 	}
 
 	void operator/=(float f)
 	{
-		x /= f;
-		y /= f;
+		m_x /= f;
+		m_y /= f;
 	}
 
 	Vec2 plus(const Vec2 &other) const
 	{
-		return Vec2(x + other.x, y + other.y);
+		return Vec2(m_x + other.m_x, m_y + other.m_y);
 	}
 
 	Vec2 minus(const Vec2 &other) const
 	{
-		return Vec2(x - other.x, y - other.y);
+		return Vec2(m_x - other.m_x, m_y - other.m_y);
 	}
 
 	void operator-=(const Vec2 &other)
 	{
-		x -= other.x;
-		y -= other.y;
+		m_x -= other.m_x;
+		m_y -= other.m_y;
 	}
 
 	void rotate(const Vec2 &origin, float angle)
@@ -86,26 +87,26 @@ struct Vec2
 		const float ca = cos(angle);
 		const float sa = sin(angle);
 
-		const float new_x = v.x * ca - v.y * sa;
-		const float new_y = v.x * sa + v.y * ca;
+		const float new_x = v.m_x * ca - v.m_y * sa;
+		const float new_y = v.m_x * sa + v.m_y * ca;
 
-		x = new_x + origin.x;
-		y = new_y + origin.y;
+		m_x = new_x + origin.m_x;
+		m_y = new_y + origin.m_y;
 	}
 
 	Vec2 getNormal() const
 	{
-		return Vec2(-y, x);
+		return Vec2(-m_y, m_x);
 	}
 
 	float dot(const Vec2 &other) const
 	{
-		return x * other.x + y * other.y;
+		return m_x * other.m_x + m_y * other.m_y;
 	}
 
 	float cross(const Vec2 &other) const
 	{
-		return x * other.y - y * other.x;
+		return m_x * other.m_y - m_y * other.m_x;
 	}
 
 	Vec2 getNormalized() const
@@ -119,20 +120,24 @@ struct Vec2
 		return v;
 	}
 
-	float x, y;
+private:
+	float m_x, m_y;
 };
 
-struct IVec2
+class IVec2
 {
-	int32_t x, y;
-
+public:
 	IVec2()
-		: x(0), y(0)
+		: m_x(0), m_y(0)
 	{
 	}
 
 	IVec2(int32_t x_, int32_t y_)
-		: x(x_), y(y_)
+		: m_x(x_), m_y(y_)
 	{
 	}
+
+private:
+	int32_t m_x, m_y;
 };
+#endif
